f107e34be340637f1b4d9b10d0e0c5df704c227f
akafc7 <fernandocavanellas6@hotmail.com>
2024-09-03 18:22:05 -0300

    Alterando JSON de faturamento

--- question3.py
+++ question3.py
@@ -8,14 +8,18 @@
 # b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
 import json
+import os
 
 def carregar_faturamento_de_arquivo(arquivo_nome):
-    with open(arquivo_nome, 'r') as arquivo:
-        dados = json.load(arquivo)
-        return dados.get("faturamento_diario", [])
+    diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+    caminho_absoluto = os.path.join(diretorio_atual, arquivo_nome)
 
-def calcular_faturamento(vetor_faturamento):
-    dias_com_faturamento = [valor for valor in vetor_faturamento if valor > 0]
+    with open(caminho_absoluto, 'r', encoding='utf-8') as arquivo:
+        dados = json.load(arquivo)
+    return dados
+
+def calcular_faturamento(dados_faturamento):
+    dias_com_faturamento = [dia['valor'] for dia in dados_faturamento if dia['valor'] > 0]
 
     menor_valor = min(dias_com_faturamento)
     maior_valor = max(dias_com_faturamento)
@@ -26,7 +30,9 @@ def calcular_faturamento(vetor_faturamento):
 
     return menor_valor, maior_valor, dias_acima_media
 
-faturamento_diario = carregar_faturamento_de_arquivo("faturamento.json")
+arquivo_nome = "dados.json"
+
+faturamento_diario = carregar_faturamento_de_arquivo(arquivo_nome)
 
 menor, maior, acima_media = calcular_faturamento(faturamento_diario)
 
@@ -34,7 +40,7 @@ print(f"Menor valor de faturamento: R${menor:.2f}")
 print(f"Maior valor de faturamento: R${maior:.2f}")
 print(f"Dias com faturamento acima da média: {acima_media}")
 
-# Saída: 
-# Menor valor de faturamento: R$100.00
-# Maior valor de faturamento: R$250.00
-# Dias com faturamento acima da média: 4
+# Saída:
+# Menor valor de faturamento: R$373.78
+# Maior valor de faturamento: R$48924.24
+# Dias com faturamento acima da média: 10
